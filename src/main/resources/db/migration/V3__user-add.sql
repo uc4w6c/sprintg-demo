db50e09f5be3b751bd5b125ac0dcaa8992347307
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-02-08 14:17:21 +0900

    db接続完了

--- src/main/resources/db/migration/V3__user-add.sql
+++ src/main/resources/db/migration/V3__user-add.sql
@@ -2,4 +2,4 @@ CREATE TABLE users(username varchar(100) NOT NULL primary key, encoded_password 
 INSERT INTO users (username, encoded_password) values ('user1', ' ');
 INSERT INTO users (username, encoded_password) values ('user2', ' ');
 ALTER TABLE customers ADD username VARCHAR(100) NOT NULL DEFAULT 'user1';
-ALTER TABLE customers ADD CONSTRAINT FK_CUSTOMERS_USERNAME FOREIGN KEY (username) REFERENCES users;
+ALTER TABLE customers ADD CONSTRAINT FK_CUSTOMERS_USERNAME FOREIGN KEY (username) REFERENCES users(username);
